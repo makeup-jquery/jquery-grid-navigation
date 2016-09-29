2a9890a55f9ef95535d8b152bf752af31e7319f4
Ian McBurnie <imcburnie@hotmail.com>
2016-09-29 09:07:57 -0700

    First version

--- jquery.gridnavigation.js
+++ jquery.gridnavigation.js
@@ -4,90 +4,257 @@
 * @version 0.0.1
 * @requires jquery
 * @requires jquery-common-keydown
+* @requires jquery-focus-exit
 */
 (function($, window, document, undefined) {
     var pluginName = 'jquery-grid-navigation';
 
     /**
     * @method "jQuery.fn.gridNavigation"
+    * @param {Object} cellsSelector - collection of navigable elements
+    * @param {Object} [options]
+    * @param {string} [options.activeIndex] - the initial active index (default: 0)
+    * @param {boolean} [options.autoInit] - init the model when plugin executes (default: true)
+    * @param {boolean} [options.autoReset] - reset the model when focus is lost (default: false)
+    * @param {boolean} [options.debug] - log debug info to console (default: false)
+    * @param {boolean} [options.wrap] - keyboard focus wraps from last to first & vice versa (default: false)
+    * @fires gridNavigationInit - when the model intialises
+    * @fires gridNavigationChange - when the current item changes
+    * @fires gridNavigationReset - when the model resets
+    * @fires gridNavigationBoundary - when a grid boundary is hit
     * @return {Object} chainable jQuery class
     */
-    $.fn.gridNavigation = function gridNavigation(itemsSelector, options) {
+    $.fn.gridNavigation = function gridNavigation(cellsSelector, options) {
         options = $.extend({
-            clickDelegate: this,
-            startCol: 0,
-            startRow: 0
+            activeIndex: 0,
+            debug: false,
+            wrap: false,
+            autoReset: false,
+            autoInit: true
         }, options);
 
         return this.each(function onEachGridNavigationWidget() {
-            var $widget = $(this);
-            var currentCol = options.startCol;
-            var currentRow = options.startRow;
-            var $clickDelegate = $(options.clickDelegate);
-            var $allItems = $widget.find(itemsSelector);
-            var $rows = $allItems.first().parent().parent().children();
-            var $cols = $rows.first().children();
-            var numCols = $cols.length;
-            var numRows = $rows.length;
+            if ($.data(this, pluginName) === undefined) {
+                var $widget = $(this);
+                var $allCells = $widget.find(cellsSelector);
+                var numCells = $allCells.length;
+                var $rows = $allCells.first().parent().parent().children();
+                var $cols = $rows.first().children();
+                var numCols = $cols.length;
+                var numRows = $rows.length;
+                var currentCol = null;
+                var currentRow = null;
 
-            $allItems.each(function(idx, itm) {
-                $.data(itm, pluginName, {index: idx});
-            });
-
-            var getCell = function(col, row) {
-                return $rows.eq(row).children().eq(col).get(0);
-            };
-
-            var updateModel = function(goToCol, goToRow) {
-                var currentCell = getCell(currentCol, currentRow);
-                var goToCell = getCell(goToCol, goToRow);
-                var currentCellData = $.data(getCell(currentCol, currentRow), pluginName);
-                var goToCellData = $.data(getCell(goToCol, goToRow), pluginName);
-
-                if (goToCol < 0 || goToCol >= numCols || goToRow < 0 || goToRow >= numRows) {
-                    console.log('Out of bounds');
-                } else {
-                    $widget.trigger("gridNavigationChange", {
-                        from: {cell: currentCell, col: currentCol, row: currentRow, index: currentCellData.index},
-                        to: {cell: goToCell, col: goToCol, row: goToRow, index: goToCellData.index}
-                    });
-                    currentCol = goToCol;
-                    currentRow = goToRow;
+                // ensure activeIndex is within bounds
+                if (options.activeIndex < 0 || options.activeIndex >= numCells) {
+                    options.activeIndex = 0;
                 }
-            };
 
-            var goToAboveItem = function(e) {
-                updateModel(currentCol, currentRow - 1);
-            };
+                var hasDoneInit = function() {
+                    return currentCol !== null && currentCol !== null;
+                };
 
-            var goToBelowItem = function(e) {
-                updateModel(currentCol, currentRow + 1);
-            };
+                var needsInit = function() {
+                    return options.autoInit === false && hasDoneInit() === false;
+                };
 
-            var goToLeftItem = function(e) {
-                updateModel(currentCol - 1, currentRow);
-            };
+                var storeData = function() {
+                    // store index position on each cell
+                    $allCells.each(function(idx, itm) {
+                        $.data(itm, pluginName, {index: idx});
+                    });
+                };
 
-            var goToRightItem = function(e) {
-                updateModel(currentCol + 1, currentRow);
-            };
+                var getCellCoords = function(cell) {
+                    var $cell = $(cell);
 
-            $widget.on('upArrowKeyDown', goToAboveItem);
-            $widget.on('downArrowKeyDown', goToBelowItem);
-            $widget.on('rightArrowKeyDown', goToRightItem);
-            $widget.on('leftArrowKeyDown', goToLeftItem);
+                    return {
+                        col: $cell.index(),
+                        row: $cell.parent().index()
+                    };
+                };
 
-            // install commonKeyDown plugin to bound element
-            $widget.commonKeyDown();
+                var getCellIndex = function(cell) {
+                    return (cell === null) ? null : $.data(cell, pluginName).index;
+                };
 
-            // listen for click events on the click delegate
-            $clickDelegate.on('click', itemsSelector, function(e) {
-                var $this = $(this);
+                var getCellByIndex = function(index) {
+                    return (index === null) ? null : $allCells.get(index);
+                };
 
-                console.log($.data(this));
+                var getCellByCoords = function(col, row) {
+                    return (col !== null && row !== null) ? $rows.eq(row).children().eq(col).get(0) : null;
+                };
 
-                updateModel($this.index(), $this.parent().index());
-            });
+                var resetModel = function() {
+                    var fromCell = getCellByCoords(currentCol, currentRow);
+                    var fromCellIndex = getCellIndex(fromCell);
+                    var eventData = {
+                        fromIndex: fromCellIndex,
+                        toIndex: null
+                    };
+
+                    currentRow = null;
+                    currentCol = null;
+
+                    $(fromCell).trigger("gridNavigationReset", eventData);
+                };
+
+                var initModel = function() {
+                    var fromCell = getCellByCoords(currentCol, currentRow);
+                    var fromCellIndex = getCellIndex(fromCell);
+                    var toCell = getCellByIndex(options.activeIndex);
+                    var toCellCoords = getCellCoords(toCell);
+                    var eventData;
+
+                    if (fromCellIndex !== options.activeIndex) {
+                        eventData = {
+                            fromIndex: fromCellIndex,
+                            toIndex: options.activeIndex
+                        };
+
+                        currentCol = toCellCoords.col;
+                        currentRow = toCellCoords.row;
+
+                        $(toCell).trigger("gridNavigationInit", eventData);
+                    }
+                };
+
+                var onFocusExit = function(e) {
+                    if (currentRow !== null && currentCol !== null) {
+                        if (options.autoInit === false) {
+                            resetModel();
+                        } else {
+                            initModel();
+                        }
+                    }
+                };
+
+                var updateModelByCoords = function(goToCol, goToRow) {
+                    var currentCell = getCellByCoords(currentCol, currentRow);
+                    var currentCellData = $.data(currentCell, pluginName);
+                    var goToCell;
+                    var goToCellData;
+                    var boundary;
+                    var eventData;
+
+                    if (goToCol < 0) {
+                        boundary = "left";
+                    } else if (goToCol >= numCols) {
+                        boundary = "right";
+                    } else if (goToRow < 0) {
+                        boundary = "top";
+                    } else if (goToRow >= numRows) {
+                        boundary = "bottom";
+                    }
+
+                    if (boundary === undefined) {
+                        goToCell = getCellByCoords(goToCol, goToRow);
+                        goToCellData = $.data(goToCell, pluginName);
+
+                        eventData = {
+                            fromIndex: currentCellData.index,
+                            toIndex: goToCellData ? goToCellData.index : null
+                        };
+
+                        $(goToCell).trigger("gridNavigationChange", eventData);
+
+                        currentCol = goToCol;
+                        currentRow = goToRow;
+                    } else {
+                        eventData = {
+                            fromIndex: currentCellData.index,
+                            toIndex: null,
+                            boundary: boundary
+                        };
+                        $(currentCell).trigger("gridNavigationBoundary", eventData);
+                    }
+                };
+
+                var updateModelByCol = function(colIndex) {
+                    updateModelByCoords(colIndex, currentRow);
+                };
+
+                var updateModelByRow = function(rowIndex) {
+                    updateModelByCoords(currentCol, rowIndex);
+                };
+
+                var updateModelByIndex = function(index) {
+                    var $cell = $($allCells.get(index));
+                    numCells = $allCells.length;
+                    updateModelByCoords($cell.index(), $cell.parent().index());
+                };
+
+                var onGridNavigationItemsChange = function() {
+                    $allCells = $widget.find(cellsSelector);
+                    numCells = $allCells.length;
+                    $rows = $allCells.first().parent().parent().children();
+                    $cols = $rows.first().children();
+                    numCols = $cols.length;
+                    numRows = $rows.length;
+                    storeData();
+                };
+
+                var onArrowKeyDown = function(e) {
+                    if (needsInit() === true) {
+                        initModel();
+                    } else {
+                        switch (e.type) {
+                            case "upArrowKeyDown":
+                                updateModelByRow(currentRow - 1);
+                                break;
+                            case "downArrowKeyDown":
+                                updateModelByRow(currentRow + 1);
+                                break;
+                            case "leftArrowKeyDown":
+                                updateModelByCol(currentCol - 1);
+                                break;
+                            case "rightArrowKeyDown":
+                                updateModelByCol(currentCol + 1);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                };
+
+                // install commonKeyDown plugin to bound element
+                $widget.commonKeyDown();
+
+                // listen for arrow key events
+                $widget.on('upArrowKeyDown downArrowKeyDown rightArrowKeyDown leftArrowKeyDown', onArrowKeyDown);
+
+                // delegate cell click events to the widget
+                $widget.on('click', cellsSelector, function(e) {
+                    var $this = $(this);
+                    if (needsInit() === true) {
+                        initModel();
+                    }
+                    updateModelByCoords($this.index(), $this.parent().index());
+                });
+
+                if (options.autoReset === true) {
+                    $widget.focusExit();
+                    $widget.on('focusExit', onFocusExit);
+                }
+
+                // handle DOM update of navigation items
+                $widget.on('gridNavigationItemsChange', onGridNavigationItemsChange);
+
+                // we can set the intial active element if arrow key not required
+                if (options.autoInit === true) {
+                    setTimeout(function() {
+                        initModel();
+                    }, 0);
+                }
+
+                // store data on grid element
+                jQuery.data(this, pluginName, {installed: 'true'});
+
+                storeData();
+            } else if (options.debug === true) {
+                console.log('debug: {pluginName} is already installed on {element}'.replace('{pluginName}', pluginName).replace('{element}', this));
+            }
         });
     };
 }(jQuery, window, document));
@@ -96,4 +263,45 @@
 * The jQuery plugin namespace.
 * @external "jQuery.fn"
 * @see {@link http://learn.jquery.com/plugins/|jQuery Plugins}
+*/
+
+/**
+* gridNavigationChange event
+* @event gridNavigationChange
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* gridNavigationReset event
+* @event gridNavigationReset
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* gridNavigationInit event
+* @event gridNavigationInit
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
+*/
+
+/**
+* gridNavigationBoundary event
+* @event gridNavigationBoundary
+* @type {object}
+* @property {object} event - event object
+* @property {object} data - event data params
+* @param {string} [data.boundary] - top, bottom, left or right
+* @param {string} [data.fromIndex] - old collection idx position
+* @param {string} [data.toIndex] - new collection idx position
 */
